76ab1bcd9cac55dab902a80ff16634db7564ca09
Pengju Zhao <109416356+PengjuZ@users.noreply.github.com>
2026-07-15 21:07:01 +0800

    Add files via upload

--- scripts/batch_mode.sh
+++ scripts/batch_mode.sh
@@ -22,38 +22,37 @@ current_path=$(pwd) # && echo ${current_path}
 # echo ${bam_file} ${list_bam_files} ${te_gff} ${output_prefix} ${thread} ${overlap} ${expectation} ${max_iterations_outer} ${max_iterations_inner} ${abe_step} ${SCRIPT_DIR} ${PARENT_DIR} ${reserve_tmp_dir} | awk '{for(i=1;i<=NF;i++) printf "\t%s\n", $i}'
 
 # invoking base model
-# echo -e ${list_bam_files} 
-echo -e ${list_bam_files} | xargs -I {} -P ${thread} ${PARENT_DIR}/scripts/base_mode.sh {} "BatchMode" ${te_gff} ${output_prefix} ${thread} ${overlap} ${expectation} ${max_iterations_outer} ${max_iterations_inner} ${abe_step} ${SCRIPT_DIR} ${PARENT_DIR} ${reserve_tmp_dir}
+cat ${list_bam_files} | xargs -I {} -P ${thread} ${PARENT_DIR}/scripts/base_mode.sh {} "BatchMode" ${te_gff} ${output_prefix} ${thread} ${overlap} ${expectation} ${max_iterations_outer} ${max_iterations_inner} ${abe_step} ${SCRIPT_DIR} ${PARENT_DIR} ${reserve_tmp_dir}
 
 # union RowName
-ls ${output_prefix}*_LATTE_TE_Expression | head -n 1 | xargs -i awk -v FS="\t" 'NR>1 {print $1}' {} > ${output_prefix}_LATTE_ReadCount
-ls ${output_prefix}*_LATTE_TE_Expression | head -n 1 | xargs -i awk -v FS="\t" 'NR>1 {print $1}' {} > ${output_prefix}_LATTE_TPM
-[[ -f ${output_prefix}_LATTE_ColName ]] && rm ${output_prefix}_LATTE_ColName
+head -n1 ${list_bam_files} | awk -v prefix=${output_prefix} -F'/' '{print prefix$NF"_LATTE_TE_Expression"}' | xargs -I {} awk -v FS="\t" 'NR>1 {print $1}' {} > ${output_prefix}LATTE_ReadCount
+head -n 1 ${list_bam_files} | awk -v prefix=${output_prefix} -F'/' '{print prefix$NF"_LATTE_TE_Expression"}' | xargs -I {} awk -v FS="\t" 'NR>1 {print $1}' {} > ${output_prefix}LATTE_TPM
+[[ -f ${output_prefix}LATTE_ColName ]] && rm ${output_prefix}LATTE_ColName
 
 # build Matrix
-echo -e ${list_bam_files} | while read AbsPath_bam_file   
+cat ${list_bam_files} | while read AbsPath_bam_file   
 do
     # 
     sample_name=$(basename $AbsPath_bam_file)
-    echo $sample_name >> ${output_prefix}_LATTE_ColName
+    echo $sample_name >> ${output_prefix}LATTE_ColName
     # 纵向合并
-    awk -v FS="\t" 'NR>1 {print $2}' ${output_prefix}${sample_name}_LATTE_TE_Expression > ${output_prefix}tmpLATTE_ReadCount
-    paste -d "\t" ${output_prefix}_LATTE_ReadCount ${output_prefix}tmpLATTE_ReadCount > ${output_prefix}tmp_LATTE && mv ${output_prefix}tmp_LATTE ${output_prefix}_LATTE_ReadCount
-    awk -v FS="\t" 'NR>1 {print $3}' ${output_prefix}${sample_name}_LATTE_TE_Expression > ${output_prefix}tmpLATTE_TPM
-    paste -d "\t" ${output_prefix}_LATTE_TPM ${output_prefix}tmpLATTE_TPM > ${output_prefix}tmp_LATTE && mv ${output_prefix}tmp_LATTE ${output_prefix}_LATTE_TPM
+    awk -v FS="\t" 'NR>1 {print $2}' "${output_prefix}${sample_name}_LATTE_TE_Expression" > ${output_prefix}tmpLATTE_ReadCount
+    paste -d "\t" ${output_prefix}LATTE_ReadCount ${output_prefix}tmpLATTE_ReadCount > ${output_prefix}tmp_LATTE && mv ${output_prefix}tmp_LATTE ${output_prefix}LATTE_ReadCount
+    awk -v FS="\t" 'NR>1 {print $3}' "${output_prefix}${sample_name}_LATTE_TE_Expression" > ${output_prefix}tmpLATTE_TPM
+    paste -d "\t" ${output_prefix}LATTE_TPM ${output_prefix}tmpLATTE_TPM > ${output_prefix}tmp_LATTE && mv ${output_prefix}tmp_LATTE ${output_prefix}LATTE_TPM
     # rm
-    rm ${output_prefix}${sample_name}_LATTE_TE_Expression
+    rm "${output_prefix}${sample_name}_LATTE_TE_Expression"
 done
 
 # 删除中间文件
 rm ${output_prefix}tmpLATTE_ReadCount ${output_prefix}tmpLATTE_TPM
 
 # process the output file
-awk '{printf "%s\t", $0}' ${output_prefix}_LATTE_ColName | awk '{gsub(/\t$/, "\n", $0); printf "\t%s", $0}'  > ${output_prefix}tmp_LATTE && mv ${output_prefix}tmp_LATTE ${output_prefix}_LATTE_ColName
-awk -v OFS="\t" '{for(i=1;i<=NF;i++){ if(i==1){printf "`%s`\t", $i}else{printf "%s%s", $i, (i == NF ? "\n" : OFS) }} }' ${output_prefix}_LATTE_TPM > ${output_prefix}tmp_LATTE && mv ${output_prefix}tmp_LATTE ${output_prefix}_LATTE_TPM
-awk -v OFS="\t" '{for(i=1;i<=NF;i++){ if(i==1){printf "`%s`\t", $i}else{printf "%s%s", $i, (i == NF ? "\n" : OFS) }} }' ${output_prefix}_LATTE_ReadCount > ${output_prefix}tmp_LATTE && mv ${output_prefix}tmp_LATTE ${output_prefix}_LATTE_ReadCount
+awk '{printf "%s\t", $0}' ${output_prefix}LATTE_ColName | awk '{gsub(/\t$/, "\n", $0); printf "\t%s", $0}'  > ${output_prefix}tmp_LATTE && mv ${output_prefix}tmp_LATTE ${output_prefix}LATTE_ColName
+awk -v OFS="\t" '{for(i=1;i<=NF;i++){ if(i==1){printf "`%s`\t", $i}else{printf "%s%s", $i, (i == NF ? "\n" : OFS) }} }' ${output_prefix}LATTE_TPM > ${output_prefix}tmp_LATTE && mv ${output_prefix}tmp_LATTE ${output_prefix}LATTE_TPM
+awk -v OFS="\t" '{for(i=1;i<=NF;i++){ if(i==1){printf "`%s`\t", $i}else{printf "%s%s", $i, (i == NF ? "\n" : OFS) }} }' ${output_prefix}LATTE_ReadCount > ${output_prefix}tmp_LATTE && mv ${output_prefix}tmp_LATTE ${output_prefix}LATTE_ReadCount
 
 # format for eQTL mapping
-cat ${output_prefix}_LATTE_ColName ${output_prefix}_LATTE_TPM > ${output_prefix}tmp_LATTE && mv ${output_prefix}tmp_LATTE ${output_prefix}_LATTE_TPM
-cat ${output_prefix}_LATTE_ColName ${output_prefix}_LATTE_ReadCount > ${output_prefix}tmp_LATTE && mv ${output_prefix}tmp_LATTE ${output_prefix}_LATTE_ReadCount
+cat ${output_prefix}LATTE_ColName ${output_prefix}LATTE_TPM > ${output_prefix}tmp_LATTE && mv ${output_prefix}tmp_LATTE ${output_prefix}LATTE_TPM
+cat ${output_prefix}LATTE_ColName ${output_prefix}LATTE_ReadCount > ${output_prefix}tmp_LATTE && mv ${output_prefix}tmp_LATTE ${output_prefix}LATTE_ReadCount
 
